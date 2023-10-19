1d36bbb842b59fb3a21feb86421af7e36e8625ef
Daniel Depoe <89668578+dan-montelo@users.noreply.github.com>
2023-10-18 23:34:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,66 +1,29 @@
-from enum import Enum
-from typing import Dict, List, Set
-
 import streamlit as st
-from pydantic import BaseModel, Field
-from pydantic.color import Color
+from pydantic import BaseModel
 
 import streamlit_pydantic as sp
 
 
-class OtherData(BaseModel):
-    text: str = "default_text"
-    integer: int = 99
-
-
-class SelectionValue(str, Enum):
-    FOO = "foo"
-    BAR = "bar"
-
-
 class ExampleModel(BaseModel):
-    """
-    A model to showcase and test different types of pydantic fields with default values"
-    """
-
-    long_text: str = Field(
-        "default string", format="multi-line", description="Unlimited text property"
-    )
-    integer_in_range: int = Field(
-        22,
-        ge=10,
-        le=30,
-        multiple_of=2,
-        description="Number property with a limited range",
-    )
-    single_selection: SelectionValue = Field(
-        "bar", description="Only select a single item from a set."
-    )
-    multi_selection: Set[SelectionValue] = Field(
-        "bar", description="Allows multiple items from a set."
-    )
-    read_only_text: str = Field(
-        "Lorem ipsum dolor sit amet",
-        description="This is ready only text.",
-        readOnly=True,
-    )
-    default_color: Color = Field("yellow", description="A defaulted color")
-    default_object: OtherData = Field(
-        OtherData(),
-        description="An object embedded into the model with a default",
-    )
-    overriden_default_object: OtherData = Field(
-        OtherData(text="overridden object text", integer="12"),
-        description="Default object overrides the embedded object defaults",
-    )
-    default_dict: Dict[str, str] = {"foo": "bar"}
-    default_list: List[str] = ["foo", "bar"]
-    default_object_list: List[OtherData] = Field(
-        [OtherData()],
-        description="A list of objects with a default object in the list",
-    )
+    some_text: str
+    some_number: int
+    some_boolean: bool
 
 
-data = sp.pydantic_input(key="my_input", model=ExampleModel)
-if data:
-    st.json(data)
+from_model_tab, from_instance_tab = st.tabs(
+    ["Form inputs from model", "Form inputs from instance"]
+)
+
+with from_model_tab:
+    data = sp.pydantic_form(key="my_form", model=ExampleModel)
+    if data:
+        st.json(data.json())
+
+with from_instance_tab:
+    instance = ExampleModel(
+        some_number=999, some_boolean=True, some_text="instance text"
+    )
+
+    instance_input_data = sp.pydantic_form(key="my_form_instance", model=instance)
+    if instance_input_data:
+        st.json(instance_input_data.json())
